210e8c44e31c15bbda48a24aa310c2f458c6b292
rkdejr2321 <rkdejr2321@naver.com>
2023-04-06 21:06:37 +0900

    first commit

---
+++ src/main/java/kakao99/brainform/BrainformApplication.java
@@ -0,0 +1,13 @@
+package kakao99.brainform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrainformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrainformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kakao99/brainform/BrainformApplicationTests.java
@@ -0,0 +1,13 @@
+package kakao99.brainform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrainformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
