f853c0a87cf18f02801f417e0a9fc5e2aa2a5127
rkdejr2321 <rkdejr2321@naver.com>
2023-04-07 11:47:47 +0900

    [Add] 회원가입 DTO

--- src/main/java/kakao99/brainform/dto/MemberRegisterDTO.java
+++ src/main/java/kakao99/brainform/dto/MemberRegisterDTO.java
@@ -1,2 +1,39 @@
-package kakao99.brainform.dto;public class MemberRegisterDTO {
+package kakao99.brainform.dto;
+
+import jakarta.validation.constraints.NotNull;
+import kakao99.brainform.entity.Gender;
+import kakao99.brainform.entity.Member;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class MemberRegisterDTO implements Serializable {
+
+    @NotNull
+    private String username;
+
+    @NotNull
+    private String gender;
+
+    @NotNull
+    private int age;
+
+    @NotNull
+    private String job;
+
+    public Member toEntity() {
+        return Member.builder()
+                .username(username)
+                .gender(gender)
+                .age(age)
+                .job(job)
+                .build();
+    }
 }
