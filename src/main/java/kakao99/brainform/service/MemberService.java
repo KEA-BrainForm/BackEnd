084204dd5b2ec0aa5b5b1f238241ea76365c0793
rkdejr2321 <rkdejr2321@naver.com>
2023-04-07 11:48:47 +0900

    [Add] 회원 서비스

--- src/main/java/kakao99/brainform/service/MemberService.java
+++ src/main/java/kakao99/brainform/service/MemberService.java
@@ -1,2 +1,20 @@
-package kakao99.brainform.service;public class MemberService {
+package kakao99.brainform.service;
+
+import kakao99.brainform.dto.MemberRegisterDTO;
+import kakao99.brainform.entity.Member;
+import kakao99.brainform.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public Member register(MemberRegisterDTO dto) {
+        Member member = dto.toEntity();
+
+        return memberRepository.save(member);
+    }
 }
