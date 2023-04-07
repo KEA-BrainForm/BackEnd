7fbaa59f16500923fe6a52edafa099f847b5951c
rkdejr2321 <rkdejr2321@naver.com>
2023-04-07 11:49:12 +0900

    [Add] 회원 레포지토리

--- src/main/java/kakao99/brainform/repository/MemberRepository.java
+++ src/main/java/kakao99/brainform/repository/MemberRepository.java
@@ -1,2 +1,12 @@
-package kakao99.brainform.repository;public interface MemberRepository {
+package kakao99.brainform.repository;
+
+import kakao99.brainform.dto.MemberRegisterDTO;
+import kakao99.brainform.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Member save(Member member);
 }
