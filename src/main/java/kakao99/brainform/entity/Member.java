00ea9bcfe3b88292b60b6f8a3396cc826a86caba
Doyoung <doodoo55@gachon.ac.kr>
2023-04-30 14:54:40 +0900

    [feat] Member Entity에 Column 추가

--- src/main/java/kakao99/brainform/entity/Member.java
+++ src/main/java/kakao99/brainform/entity/Member.java
@@ -3,6 +3,10 @@ package kakao99.brainform.entity;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.util.Date;
 
 @Entity
 @Getter
@@ -20,6 +24,10 @@ public class Member {
 
     @Column
     @NotNull
+    private String nickname;
+
+    @Column
+    @NotNull
     private String gender;
 
     @Column
@@ -29,4 +37,31 @@ public class Member {
     @Column
     @NotNull
     private String job;
+
+    @Column(name = "kakao_id")
+    private String kakaoId;
+
+    @Column(name = "google_id")
+    private String googleId;
+
+    @Column(name = "naver_id")
+    private String naverId;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreationTimestamp
+    private Date createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @UpdateTimestamp
+    private Date updatedAt;
+
+
+    @Column(name = "is_active")
+    private Boolean isActive;
+
+
 }
+
+
