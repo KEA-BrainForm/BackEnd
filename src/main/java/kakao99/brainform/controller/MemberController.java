a7a0b444326713a4b0a0528022be8a611873609b
rkdejr2321 <rkdejr2321@naver.com>
2023-05-01 23:18:32 +0900

    [Feat] 회원가입 시 필수 값 검증

--- src/main/java/kakao99/brainform/controller/MemberController.java
+++ src/main/java/kakao99/brainform/controller/MemberController.java
@@ -1,16 +1,20 @@
 package kakao99.brainform.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import kakao99.brainform.dto.MemberLoginDTO;
 import kakao99.brainform.dto.MemberRegisterDTO;
 
 import kakao99.brainform.entity.Member;
 import kakao99.brainform.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import org.springframework.security.core.Authentication;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -20,11 +24,18 @@ public class MemberController {
 
     private final MemberService memberService;
 
-    @PostMapping("/api/register")
-    public ResponseEntity<?> register(@RequestBody MemberRegisterDTO dto) {
+    private final ObjectMapper objectMapper;
 
-        log.info(dto.toString());
-        memberService.register(dto);
+    @SneakyThrows
+    @PostMapping("/api/register")
+    public ResponseEntity<?> register(@RequestBody @Validated MemberRegisterDTO dto,
+                                      Authentication authentication,
+                                      BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            return new ResponseEntity<>("값을 모두 입력해주세요", HttpStatus.BAD_REQUEST);
+        }
+        memberService.join(dto, authentication);
 
         return new ResponseEntity<>("회원가입이 완료되었습니다.", HttpStatus.OK);
     }
